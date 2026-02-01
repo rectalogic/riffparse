1ee5fa013454c95f58e5b15bc0b14bfebaf8546c
Andrew Wason <rectalogic@rectalogic.com>
2026-02-01 16:35:21 -0500

    move read/skip into parser

--- src/riff.rs
+++ src/riff.rs
@@ -8,54 +8,61 @@ use core::{cell::RefCell, fmt::Debug, iter::Iterator};
 use crate::fourcc::FourCC;
 
 pub struct RiffParser<R> {
-    reader: Rc<RefCell<R>>,
+    reader: RefCell<R>,
 }
 
 impl<R: Read + Seek> RiffParser<R> {
     pub fn new(reader: R) -> Self {
         Self {
-            reader: Rc::new(RefCell::new(reader)),
+            reader: RefCell::new(reader),
         }
+    }
+
+    pub fn skip_item(&mut self, item: &mut RiffItem) -> BinResult<()> {
+        item.skip(&mut *self.reader.borrow_mut())
+    }
+
+    pub fn read_item_vec(&mut self, item: &mut RiffItem) -> BinResult<Vec<u8>> {
+        item.read_vec(&mut *self.reader.borrow_mut())
+    }
+
+    pub fn read_item(&mut self, item: &mut RiffItem, buffer: &mut [u8]) -> BinResult<()> {
+        item.read(&mut *self.reader.borrow_mut(), buffer)
     }
 }
 
 impl<R: Read + Seek> Iterator for RiffParser<R> {
-    type Item = BinResult<RiffItem<R>>;
+    type Item = BinResult<RiffItem>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let riff_type = match RiffType::read(&mut *self.reader.borrow_mut()) {
             Ok(riff_type) => riff_type,
             Err(e) => return Some(Err(e)),
         };
-        Some(Ok(RiffItem::new(Rc::clone(&self.reader), riff_type)))
+        Some(Ok(RiffItem::new(riff_type)))
 
         //XXX need a stack of riff_type, and keep track of amount read each time and pop when consumed
         //XXX Seek::stream_position
+        // caller may want the stack too so it knows where it is - api to fetch immutable view on stack
+        // also need to prevent RiffItem::skip/read from being called more than one (need to track consumed on it, so if already consumed disallow)
+        // track consumed in RiffItem, make iter return &mut RiffItem (and keep on stack) - so read/skip updates that instance
     }
 }
 
-pub struct RiffItem<R: Read + Seek> {
-    reader: Rc<RefCell<R>>,
+#[derive(Debug)]
+pub struct RiffItem {
     riff_type: RiffType,
 }
 
-impl<R: Read + Seek> Debug for RiffItem<R> {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.debug_struct("RiffItem")
-            .field("riff_type", &self.riff_type)
-            .finish()
-    }
-}
-
-impl<R: Read + Seek> RiffItem<R> {
-    fn new(reader: Rc<RefCell<R>>, riff_type: RiffType) -> Self {
-        Self { reader, riff_type }
+impl RiffItem {
+    fn new(riff_type: RiffType) -> Self {
+        Self { riff_type }
     }
 
     fn data_size(&self) -> u32 {
         match self.riff_type {
             RiffType::Chunk(chunk) => chunk.size,
-            // Subtract the size of list_id
+            // list_id is counted in size, but we already read it. So subtract from size.
             RiffType::List(list) | RiffType::Riff(list) => list.size - 4,
         }
     }
@@ -83,27 +90,23 @@ pub struct List {
     list_id: FourCC,
 }
 
-impl<R: Read + Seek> RiffItem<R> {
-    pub fn skip(&mut self) -> BinResult<()> {
+impl RiffItem {
+    fn skip<R: Read + Seek>(&mut self, reader: &mut R) -> BinResult<()> {
         let mut size = self.data_size();
         // Include padding byte
         if !size.is_multiple_of(2) {
             size += 1;
         };
-        self.reader
-            .borrow_mut()
-            .seek_relative(size as i64)
-            .map_err(binrw::Error::Io)
+        reader.seek_relative(size as i64).map_err(binrw::Error::Io)
     }
 
-    pub fn read_vec(&mut self) -> BinResult<Vec<u8>> {
+    fn read_vec<R: Read + Seek>(&mut self, reader: &mut R) -> BinResult<Vec<u8>> {
         let mut buffer = vec![0u8; self.data_size() as usize];
-        self.read(&mut buffer)?;
+        self.read(reader, &mut buffer)?;
         Ok(buffer)
     }
 
-    pub fn read(&mut self, buffer: &mut [u8]) -> BinResult<()> {
-        let mut reader = self.reader.borrow_mut();
+    fn read<R: Read + Seek>(&mut self, reader: &mut R, buffer: &mut [u8]) -> BinResult<()> {
         let size = self.data_size();
         if buffer.len() != size as usize {
             let pos = reader.stream_position().unwrap_or(0);
@@ -139,7 +142,7 @@ mod tests {
         dbg!(&riff);
         let mut hdr = parser.next().unwrap().unwrap();
         dbg!(&hdr);
-        hdr.skip().unwrap();
+        parser.skip_item(&mut hdr).unwrap();
         let chunk = parser.next().unwrap().unwrap();
         dbg!(&chunk);
     }
