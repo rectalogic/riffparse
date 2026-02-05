f6b3a3d590fc0cc29c2d7ddac75a52ac1f183b7b
Andrew Wason <rectalogic@rectalogic.com>
2026-02-05 14:33:23 -0500

    snapshot integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,29 +1,40 @@
 use core::fmt::Debug;
 use riffparse::{List, Read, Riff, RiffParser, RiffType, Seek, avi};
-use std::fs::File;
+use std::{
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
 
-fn process_list<R: Read + Seek + Debug>(list: Riff<List, R>) {
-    dbg!(&list);
+// Generate test video:
+// ffmpeg -y -f lavfi -i testsrc=size=32x24:rate=20:duration=1:decimals=3 -f lavfi -i sine=frequency=1000:sample_rate=16000 -c:v mjpeg -c:a pcm_s16le -shortest -r 20 -f avi test.avi
+
+fn debug<T: Debug, W: Write>(o: T, output: &mut W) {
+    write!(output, "{}", format_args!("{o:?}\n")).unwrap();
+}
+
+fn process_list<R: Read + Seek + Debug, W: Write>(list: Riff<List, R>, output: &mut W) {
+    debug(&list, output);
 
     let mut stream: Option<avi::AviStreamHeader> = None;
     for chunk in list.iter() {
         let chunk = chunk.unwrap();
         match chunk {
             RiffType::List(riff_list) => {
-                process_list(riff_list);
+                process_list(riff_list, output);
             }
             RiffType::Chunk(mut riff_chunk) => {
-                dbg!(&riff_chunk);
+                debug(&riff_chunk, output);
                 match riff_chunk.id() {
                     avi::tag::AVIH => {
                         let avih = riff_chunk.read_data_struct::<avi::AviMainHeader>().unwrap();
-                        dbg!(avih);
+                        debug(avih, output);
                     }
                     avi::tag::STRH => {
                         let strh = riff_chunk
                             .read_data_struct::<avi::AviStreamHeader>()
                             .unwrap();
-                        dbg!(&strh);
+                        debug(&strh, output);
                         stream = Some(strh);
                     }
                     avi::tag::STRF => {
@@ -32,12 +43,12 @@ fn process_list<R: Read + Seek + Debug>(list: Riff<List, R>) {
                                 avi::tag::VIDS => {
                                     let vids =
                                         riff_chunk.read_data_struct::<avi::BitmapInfo>().unwrap();
-                                    dbg!(vids);
+                                    debug(vids, output);
                                 }
                                 avi::tag::AUDS => {
                                     let auds =
                                         riff_chunk.read_data_struct::<avi::WaveFormat>().unwrap();
-                                    dbg!(auds);
+                                    debug(auds, output);
                                 }
                                 _ => {}
                             };
@@ -51,11 +62,26 @@ fn process_list<R: Read + Seek + Debug>(list: Riff<List, R>) {
     }
 }
 
+fn dump_avi<W: Write>(output: &mut W) {
+    let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/test.avi");
+    let file = File::open(path).unwrap();
+    let parser = RiffParser::new(file);
+    process_list(parser.riff().unwrap(), output);
+}
+
 #[test]
 fn test_avi() {
-    let file =
-        File::open("/Users/aw/Projects/rectalogic/experiments/vendor/esp32-tv/player/milk2.avi")
-            .unwrap();
-    let parser = RiffParser::new(file);
-    process_list(parser.riff().unwrap());
+    let snapshot_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/test.avi.snapshot");
+    let snapshot = fs::read_to_string(snapshot_path).unwrap();
+    let mut output = Vec::new();
+    dump_avi(&mut output);
+    assert_eq!(snapshot, String::from_utf8(output).unwrap());
+}
+
+#[test]
+#[ignore]
+fn test_avi_snapshot() {
+    let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/test.avi.snapshot");
+    let mut file = File::create(path).unwrap();
+    dump_avi(&mut file);
 }
